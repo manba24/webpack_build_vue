0a5d8b911003e7b0fc2fbb0cf3c791839a4a43fb
mjj <306459374@qq.com>
2018-07-02 17:17:44 +0800

    use webpack4

--- build/vue-loader.config.js
+++ build/vue-loader.config.js
@@ -1,13 +1,9 @@
 module.exports = (isDev) => {
-    return {
-        preserverWhitepace: true,  //去除换行的空格
-        extractCSS: !isDev,        //.vue文件里面css打包
-        cssModules: {
-            localIdentName: isDev?'[path]-[name]-[hash:base64:5]':'[hash:base64:5]',
-            camelCase: true
-        },
-        //hotReload:false,      //根据环境变量生成
-
-
+  return {
+    preserverWhitepace: true,  //去除换行的空格
+    cssModules: {
+      localIdentName: isDev ? '[path]-[name]-[hash:base64:5]' : '[hash:base64:5]',
+      camelCase: true
     }
+  }
 }
--- build/webpack.config.client.js
+++ build/webpack.config.client.js
@@ -2,105 +2,99 @@ const path = require('path')
 const HTMLPlugin = require('html-webpack-plugin')
 const webpack = require('webpack')
 const merge = require('webpack-merge')
-const ExtractPlugin = require('extract-text-webpack-plugin')
 const baseConfig = require('./webpack.config.base')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 const isDev = process.env.NODE_ENV === 'development'
 
 const defaultPlugins = [
-    new webpack.DefinePlugin({
-        'process.env': {
-            NODE_ENV: isDev ? '"development"' : '"production"'
-        }
-    }),
-    new HTMLPlugin()
+  new webpack.DefinePlugin({
+    'process.env': {
+      NODE_ENV: isDev ? '"development"' : '"production"'
+    }
+  }),
+  new HTMLPlugin()
 ]
 
 const devServer = {
-    port: 8000,
-    host: '0.0.0.0',
-    overlay: {
-        errors: true,
-    },
-    hot: true
+  port: 8000,
+  host: '0.0.0.0',
+  overlay: {
+    errors: true
+  },
+  hot: true
 }
 let config
 
 if (isDev) {
-    config = merge(baseConfig, {
-        devtool: '#cheap-module-eval-source-map',
-        module: {
-            rules: [
-                {
-                    test: /\.styl/,
-                    use: [
-                        'vue-style-loader',
-                        'css-loader',
-                        //使用cssModule配置扩展项
-                        // {
-                        //     loader:'css-loader',
-                        //     options:{
-                        //         module:true,
-                        //         localIdentName: isDev?'[path]-[name]-[hash:base64:5]':'[hash:base64:5]'
-                        //     }
-                        // },
-                        {
-                            loader: 'postcss-loader',
-                            options: {
-                                sourceMap: true,
-                            }
-                        },
-                        'stylus-loader'
-                    ]
-                }
-            ]
-        },
-        devServer,
-        plugins:defaultPlugins.concat([
-            new webpack.HotModuleReplacementPlugin(),
-            new webpack.NoEmitOnErrorsPlugin()
-        ])
-    })
+  config = merge(baseConfig, {
+    devtool: '#cheap-module-eval-source-map',
+    module: {
+      rules: [
+        {
+          test: /\.styl/,
+          use: [
+            'vue-style-loader',
+            'css-loader',
+            {
+              loader: 'postcss-loader',
+              options: {
+                sourceMap: true
+              }
+            },
+            'stylus-loader'
+          ]
+        }
+      ]
+    },
+    devServer,
+    plugins: defaultPlugins.concat([
+      new webpack.HotModuleReplacementPlugin()
+    ])
+  })
 } else {
-    config = merge(baseConfig, {
-        entry: {
-            app: path.join(__dirname, '../client/index.js'),
-            vendor: ['vue']
+  config = merge(baseConfig, {
+    entry: {
+      app: path.join(__dirname, '../client/index.js')
+      // vendor: ['vue']
+    },
+    output: {
+      filename: '[name].[chunkhash:8].js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.css/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            'css-loader',
+            'postcss-loader'
+          ]
         },
-        output: {
-            filename: '[name].[chunkhash:8].js'
-        },
-        module: {
-            rules: [
-                {
-                    test: /\.styl/,
-                    use: ExtractPlugin.extract({
-                        fallback: 'vue-style-loader',
-                        use: [
-                            'css-loader',
-                            {
-                                loader: 'postcss-loader',
-                                options: {
-                                    sourceMap: true,
-                                }
-                            },
-                            'stylus-loader'
-                        ]
-                    })
-                },
-            ]
-        },
-        plugins:defaultPlugins.concat([
-            new ExtractPlugin('styles.[contentHash:8].css'),
-            new webpack.optimize.CommonsChunkPlugin({
-                name: 'vendor'
-            }),
-            new webpack.optimize.CommonsChunkPlugin({
-                name: 'runtime'
-            })
-        ])
-    })
-
+        {
+          test: /\.styl/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            'css-loader',
+            'postcss-loader',
+            'stylus-loader'
+          ]
+        }
+      ]
+    },
+    optimization: {
+      splitChunks: {
+        chunks: 'all'
+      },
+      runtimeChunk: true
+    },
+    plugins: defaultPlugins.concat([
+      new MiniCssExtractPlugin({
+        filename: '[name].[hash].css',
+        chunkFilename: '[id].[contenthash:8].css'
+      })
+    ])
+  })
 }
 
 module.exports = config
